ccc05a7b4bdb53dce05827bef49bb6a6fa69ae30
Kittypoda <odakrolstad@gmail.com>
2024-05-27 12:51:09 +0200

    logout js + css

--- scripts/admin.mjs
+++ scripts/admin.mjs
@@ -1,3 +1,11 @@
 import { renderHomePage } from "./index.mjs";
 
 renderHomePage(false,true);
+
+document.getElementById('logout-button').addEventListener('click', () => {
+  // Clear local storage
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('userName');
+  // Redirect to the login page
+  window.location.href = './login.html';
+});
